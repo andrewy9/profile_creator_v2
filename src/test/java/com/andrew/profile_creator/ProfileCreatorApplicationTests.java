43742b4302c3c2a526d713cfebb29166a4c46c52
andrewy9 <andrew.yang993@gmail.com>
2022-04-16 12:02:40 +1200

    initial commit

---
+++ src/test/java/com/andrew/profile_creator/ProfileCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrew.profile_creator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
